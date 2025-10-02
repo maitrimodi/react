d390891f64c1ea7e6079c20f43b1181e7bbc8972
Maitri Modi <maitrimodi19@gmail.com>
2025-10-02 13:43:28 -0700

    Ignited the React app

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
-// ReactElement(Object) => HTML(Browser understands)
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 
 const parent = React.createElement('div', { id: 'parent' }, [
   React.createElement('div', { id: 'child' }, [
-    React.createElement('h1', {}, "I'm an h1 tag"),
-    React.createElement('h2', {}, "I'm an h2 tag"),
+    React.createElement('h1', {}, 'This is React Brushup ⏣'),
+    React.createElement('h2', {}, 'by Maitri Modi'),
   ]),
   React.createElement('div', { id: 'child2' }, [
     React.createElement('h1', {}, "I'm an h1 tag"),
@@ -12,12 +13,6 @@ const parent = React.createElement('div', { id: 'parent' }, [
 ]);
 
 // JSX
-
-const heading = React.createElement(
-  'h1',
-  { id: 'heading' },
-  'Hello World from React!'
-);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# Namaste React
+# React Brushup ⏣
+
+# Parcel
+
+- Dev Build
+- Local Server
+- HMR - Hot Module Reload
+- File Watching Algorithm - written in C++
+- Caching - Faster Builds
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Content Hashing
+- Code Splitting
+- Differential Bundling - support older browsers
+- Diagnostic
+- Error Handling
+- HTTPs
+- Tree Shaking - remove unused code
+- Transpilation
+- Different build for dev and prod bundles
